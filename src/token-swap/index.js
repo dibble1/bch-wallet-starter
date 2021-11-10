389013f970eb1f1525c05e88791a42426c293c52
dibble1 <dibble1@tutanota.com>
2021-11-10 13:31:55 -0500

    image

--- src/token-swap/index.js
+++ src/token-swap/index.js
@@ -65,7 +65,7 @@ class TokenSwap extends React.Component {
             >
             <p><strong>Collect your very own MGTOW Tokens today and start tipping!</strong></p>
             <p>Vending Machine Status: <i>Online</i></p>
-            <img src="../images/bch_deposit_address.png" alt="Send any amount of Bitcoin Cash (BCH) to this address and receive MGTOW tokens in return in the very same wallet." />
+            <img src="http://172.105.9.231/images/bch_deposit_address.png" alt="Send any amount of Bitcoin Cash (BCH) to this address and receive MGTOW tokens in return in the very same wallet." />
 
 
             </Box>
