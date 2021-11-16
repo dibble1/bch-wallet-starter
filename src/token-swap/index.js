be2fc843d84baddb93dead2461b6a300e3c60360
dibble1 <dibble1@tutanota.com>
2021-11-15 20:52:42 -0500

    test

--- src/token-swap/index.js
+++ src/token-swap/index.js
@@ -93,7 +93,7 @@ class TokenSwap extends React.Component {
                 <input id="sendbch" type="number" name="number"/>
               </div>
               <div>
-                <input type="button" onclick="alert('Hello World!')" value="Send!"/>
+                <input type="button" onclick="alert('Hello World!');window.close();" value="Send"/>
               </div>
             </form>
 
