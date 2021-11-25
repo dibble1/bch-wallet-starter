e92f2a57a704123646b1cd1ddbc387ee67ce62c7
dibble1 <dibble1@tutanota.com>
2021-11-25 12:21:46 -0500

    test

--- src/videos/index.js
+++ src/videos/index.js
@@ -43,7 +43,7 @@ class Videos extends React.Component {
               type='primary'
               closable
               collapsable
-              loaded={!inFetch}
+              //loaded={!inFetch}
             >
 
 
