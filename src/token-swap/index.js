759658237b68c02f872d9a6673a2529689f8ea4f
dibble1 <dibble1@tutanota.com>
2021-11-25 08:42:15 -0500

    test

--- src/token-swap/index.js
+++ src/token-swap/index.js
@@ -102,7 +102,7 @@ class TokenSwap extends React.Component {
 
                // Here the value is stored in new variable x
                function myFunction() {
-                   var x = document.getElementById("myText").value;
+                   const x = document.getElementById("myText").value;
                    document.getElementById("demo").innerHTML = x;
                }
                </script>
