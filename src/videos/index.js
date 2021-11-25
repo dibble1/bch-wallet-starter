42857340f29f46da9c6b5a569b8f46bd7ca3030c
dibble1 <dibble1@tutanota.com>
2021-11-25 15:10:51 -0500

    test

--- src/videos/index.js
+++ src/videos/index.js
@@ -11,14 +11,24 @@ const BchWallet = typeof window !== 'undefined' ? window.SlpWallet : null
 
 
 let _this
-class Videos extends React.Component {
+class Send extends React.Component {
   constructor (props) {
     super(props)
-    _this = this
-    this.state = {
-      test: 0
 
+    _this = this
+
+    this.state = {
+      address: '',
+      amountSat: '',
+      errMsg: '',
+      txId: '',
+      showScan: false,
+      inFetch: false,
+      sendCurrency: 'USD',
+      sendMax: false,
+      explorerURL: ''
     }
+    _this.BchWallet = BchWallet
   }
 
   render () {
