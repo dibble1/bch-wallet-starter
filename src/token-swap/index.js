d3c6de823d2d0e96e243cc162b649e14291a6fa3
dibble1 <dibble1@tutanota.com>
2021-11-26 14:59:05 -0500

    test

--- src/token-swap/index.js
+++ src/token-swap/index.js
@@ -1,25 +1,15 @@
-/*
-  This is a demonstration component. It helps to show how you can create new
-  menu items and Views in your own BCH web wallet dashboard app.
-
-  This file controls the View (the part on the right side of the dashboard) of
-  the component. The menu item is controlled by the menu-components.js file.
-*/
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Content, Row, Col, Box, Inputs, Button } from 'adminlte-2-react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
-import { getWalletInfo } from 'gatsby-ipfs-web-wallet/src/components/localWallet'
 import bchImage from '../images/bch_deposit_address.png'
 import fullStackLogo from '../images/fullstack-logo.png'
 import zapitLogo from '../images/zapit-logo.png'
 import bitcoincomLogo from '../images/bitcoincom-logo.jpeg'
 import memocashLogo from '../images/memocash-logo.jpeg'
-
-//const BchWallet =
-//  typeof window !== 'undefined'
-//    ? window.SlpWallet
-//    : null
+// import BchWallet from 'minimal-slp-wallet'
+//import ScannerModal from '../../../gatsby-ipfs-web-wallet/src/components/qr-scanner/modal'
+const { Text } = Inputs
 
 const BchWallet = typeof window !== 'undefined' ? window.SlpWallet : null
 
@@ -27,51 +17,119 @@ let _this
 class TokenSwap extends React.Component {
   constructor (props) {
     super(props)
-    _this = this
-    this.state = {
-      unconfirmedBalance: 0,
-      confirmedBalance: 0,
-      totalBalance: 0,
-      inFetch: false,
-      bchWallet: '',
-      isChecked: ''
 
+    _this = this
+
+    this.state = {
+      address: 'bitcoincash:qpsm6kdg8382ml3d03mtyp6pt4wmpz4q2smpxlhqzj',
+      amountSat: '',
+      errMsg: '',
+      txId: '',
+      showScan: false,
+      inFetch: false,
+      sendCurrency: 'USD',
+      sendMax: false,
+      explorerURL: ''
     }
+    _this.BchWallet = BchWallet
   }
 
   render () {
-    const {
-      unconfirmedBalance,
-      confirmedBalance,
-      totalBalance,
-      inFetch,
-      isChecked
-    } = _this.state
     return (
-      <Content
-        title='Token Swap'
-        //featuredImage: ../images/bch_deposit_address.png
-        subTitle='Swap BCH For MGTOW'
-        browserTitle='Token Swap'
-      >
-        <Row>
-          <Col xs={22}>
-            <Box
-              title='MGTOW Token Vending Machine'
-              type='primary'
-              closable
-              collapsable
-              loaded={!inFetch}
-              //footer={
-                //<Button
-                //  type='primary'
-                //  text='Check Balance'
-                //  onClick={_this.handleGetBalance}
-                ///>
-              //}
-            >
-            <p><strong>Collect your very own MGTOW Tokens today and start tipping!</strong></p>
-            <p>Vending Machine Status: <i>Online</i></p>
+      <>
+        <Content>
+          <Row>
+            <Col sm={2} />
+            <Col sm={22}>
+              <Box
+              title='Swap BCH For MGTOW'
+                loaded={!_this.state.inFetch}
+                closable
+                collapsable
+              >
+                <Row>
+                  <Col sm={12} className='text-center'>
+                    <h1>
+                      <FontAwesomeIcon
+                        className='title-icon'
+                      />
+                    </h1>
+                    <Box className='border-none'>
+                      <Text
+                        id='amountToSend'
+                        name='amountSat'
+                        value={_this.state.amountSat}
+                        placeholder={`Enter amount to send in ${_this.state.sendCurrency}`}
+                        label='bitcoincash:qpsm6kdg8382ml3d03mtyp6pt4wmpz4q2smpxlhqzj'
+                        labelPosition='above'
+                        onChange={_this.handleUpdate}
+                        addonRight={_this.state.sendCurrency}
+                        disabled={_this.state.sendMax}
+                        buttonRight={
+                          <Button
+                            icon='fa-random'
+                            onClick={_this.handleChangeCurrency}
+                          />
+                        }
+                        buttonLeft={
+                          <Button
+                            text={_this.state.sendMax ? 'UNDO' : 'MAX'}
+                            onClick={_this.handleSendType}
+                          />
+                        }
+                      />
+                      <div className='text-left pb-4'>
+                        <p>
+                          {_this.state.sendCurrency === 'BCH'
+                            ? `USD: ${(
+                                _this.state.amountSat *
+                                (_this.props.currentRate / 100)
+                              ).toFixed(2)}`
+                            : `BCH: ${(
+                                _this.state.amountSat /
+                                (_this.props.currentRate / 100)
+                              ).toFixed(8)}`}
+                        </p>
+                      </div>
+                      <Button
+                        text='Send'
+                        type='primary'
+                        className='btn-lg'
+                        onClick={
+                          _this.state.sendMax
+                            ? _this.handleSendAll
+                            : _this.handleSend
+                        }
+                      />
+                    </Box>
+                  </Col>
+                  <Col sm={12} className='text-center'>
+                    {_this.state.errMsg && (
+                      <p className='error-color'>{_this.state.errMsg}</p>
+                    )}
+                    {_this.state.txId && (
+                      <a
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        href={`${_this.state.explorerURL}/${_this.state.txId}`}
+                      >
+                        Transaction ID: {_this.state.txId}
+                      </a>
+                    )}
+                  </Col>
+                </Row>
+              </Box>
+            </Col>
+            <Col sm={2} />
+          </Row>
+
+          <Box
+            title='Use Your Own Wallet'
+            type='primary'
+            closable
+            collapsable
+            //loaded={!inFetch}
+          >
 
             <table>
                <tr>
@@ -89,28 +147,378 @@ class TokenSwap extends React.Component {
                   <td>https://memo.cash</td>
                </tr>
             </table>
+          >
+          </Box>
 
-
-            <form>
-              <label>
-                Send BCH($):
-                <input
-                  name="sendBCH"
-                  type="number"
-                  step="any"
-                  value={this.state.sendBCH}
-                  onChange={this.handleInputChange} />
-                  <button type="submit">Send</button>
-              </label>
-            </form>
-            </Box>
-          </Col>
-        </Row>
-      </Content>
+        </Content>
+      </>
     )
   }
 
+  componentDidMount () {
+    const { bchWallet } = _this.props
+    console.log('bchWallet', bchWallet)
+    _this.defineExplorer()
+  }
 
+  // Define the explorer to use
+  // depending on the selected chain
+  defineExplorer () {
+    const bchWalletLib = _this.props.bchWallet
+    const bchjs = bchWalletLib.bchjs
+
+    let explorerURL
+
+    if (bchjs.restURL.includes('abc.fullstack')) {
+      explorerURL = 'https://explorer.bitcoinabc.org/tx'
+    } else {
+      explorerURL = 'https://explorer.bitcoin.com/bch/tx'
+    }
+    _this.setState({
+      explorerURL
+    })
+  }
+
+  handleChangeCurrency () {
+    if (_this.state.sendCurrency === 'USD') {
+      _this.setState({
+        sendCurrency: 'BCH'
+      })
+      if (_this.state.amountSat > 0) {
+        _this.setState({
+          amountSat: (
+            _this.state.amountSat /
+            (_this.props.currentRate / 100)
+          ).toFixed(8)
+        })
+      }
+    } else {
+      _this.setState({
+        sendCurrency: 'USD'
+      })
+      if (_this.state.amountSat > 0) {
+        _this.setState({
+          amountSat: (
+            _this.state.amountSat *
+            (_this.props.currentRate / 100)
+          ).toFixed(2)
+        })
+      }
+    }
+  }
+
+  handleSendType () {
+    const sendMax = !_this.state.sendMax
+    _this.setState({
+      sendMax
+    })
+    if (sendMax) {
+      _this.getMaxAmount()
+    } else {
+      _this.setState({
+        amountSat: ''
+      })
+    }
+  }
+
+  async getMaxAmount () {
+    try {
+      const bchWalletLib = _this.props.bchWallet
+      // Ensure the wallet UTXOs are up-to-date.
+      const walletAddr = bchWalletLib.walletInfo.address
+      await bchWalletLib.utxos.initUtxoStore(walletAddr)
+
+      const utxos = bchWalletLib.utxos.utxoStore.bchUtxos
+      if (!utxos.length) {
+        throw new Error('No BCH Utxos to spend!')
+      }
+
+      // Get total of satoshis fron the bch utxos
+      let totalAmount = 0
+      utxos.map(val => (totalAmount += val.value))
+
+      // Convert satoshis to bch
+      let amountSat = totalAmount / 100000000
+
+      // Change the amount to send to USD if is the selected currency
+      if (_this.state.sendCurrency === 'USD') {
+        const _usdAmount = amountSat * (_this.props.currentRate / 100)
+        const usdAmount = Number(_usdAmount.toFixed(2)) // usd Amount
+        amountSat = usdAmount
+      }
+
+      _this.setState({
+        amountSat: amountSat
+      })
+    } catch (error) {
+      console.error(error)
+      _this.setState({
+        errMsg: error.message,
+        sendMax: false
+      })
+    }
+  }
+
+  async handleSendAll () {
+    try {
+      _this.validateInputs()
+
+      const bchWalletLib = _this.props.bchWallet
+      let { address, amountSat } = _this.state
+
+      if (_this.state.sendCurrency === 'USD') {
+        amountSat = (amountSat / (_this.props.currentRate / 100)).toFixed(8)
+      }
+
+      const amountToSend = Math.floor(Number(amountSat) * 100000000)
+      console.log(`Sending ${amountToSend} satoshis to ${address}`)
+
+      if (!bchWalletLib) {
+        throw new Error('Wallet not found')
+      }
+      _this.setState({
+        inFetch: true
+      })
+
+      // Ensure the wallet UTXOs are up-to-date.
+      const walletAddr = bchWalletLib.walletInfo.address
+      await bchWalletLib.utxos.initUtxoStore(walletAddr)
+
+      // Send the BCH.
+      const result = await bchWalletLib.sendAll(address)
+      // console.log('result',result)
+
+      _this.setState({
+        txId: result
+      })
+
+      // update balance
+      setTimeout(async () => {
+        const myBalance = await bchWalletLib.getBalance()
+        const bchjs = bchWalletLib.bchjs
+
+        let currentRate
+
+        if (bchjs.restURL.includes('abc.fullstack')) {
+          currentRate = (await bchjs.Price.getBchaUsd()) * 100
+        } else {
+          // BCHN price.
+          currentRate = (await bchjs.Price.getUsd()) * 100
+        }
+
+        _this.props.updateBalance({ myBalance, currentRate })
+      }, 1000)
+
+      _this.resetValues()
+    } catch (error) {
+      _this.handleError(error)
+    }
+  }
+
+  handleUpdate (event) {
+    const value = event.target.value
+    _this.setState({
+      [event.target.name]: value
+    })
+  }
+
+  async handleSend () {
+    try {
+      _this.validateInputs()
+
+      const bchWalletLib = _this.props.bchWallet
+      let { address, amountSat } = _this.state
+
+      if (_this.state.sendCurrency === 'USD') {
+        amountSat = (amountSat / (_this.props.currentRate / 100)).toFixed(8)
+      }
+
+      const amountToSend = Math.floor(Number(amountSat) * 100000000)
+      console.log(`Sending ${amountToSend} satoshis to ${address}`)
+
+      const receivers = [
+        {
+          address,
+          // amount in satoshis, 1 satoshi = 0.00000001 Bitcoin
+          amountSat: amountToSend
+        }
+      ]
+      // console.log("receivers", receivers)
+
+      if (!bchWalletLib) {
+        throw new Error('Wallet not found')
+      }
+      _this.setState({
+        inFetch: true
+      })
+
+      // Ensure the wallet UTXOs are up-to-date.
+      const walletAddr = bchWalletLib.walletInfo.address
+      await bchWalletLib.utxos.initUtxoStore(walletAddr)
+
+      // Used for debugging.
+      // console.log(
+      //   `bchWalletLib.utxos.bchUtxos: ${JSON.stringify(
+      //     bchWalletLib.utxos.bchUtxos,
+      //     null,
+      //     2
+      //   )}`
+      // )
+
+      // Send the BCH.
+      const result = await bchWalletLib.send(receivers)
+      console.log('result', result)
+
+      _this.setState({
+        txId: result.txid || result
+      })
+
+      // update balance
+      setTimeout(async () => {
+        const myBalance = await bchWalletLib.getBalance()
+        const bchjs = bchWalletLib.bchjs
+
+        let currentRate
+
+        if (bchjs.restURL.includes('abc.fullstack')) {
+          currentRate = (await bchjs.Price.getBchaUsd()) * 100
+        } else {
+          // BCHN price.
+          currentRate = (await bchjs.Price.getUsd()) * 100
+        }
+
+        _this.props.updateBalance({ myBalance, currentRate })
+      }, 1000)
+
+      _this.resetValues()
+    } catch (error) {
+      _this.handleError(error)
+    }
+  }
+
+  // Reset form and component state
+  resetValues () {
+    _this.setState({
+      address: 'bitcoincash:qpsm6kdg8382ml3d03mtyp6pt4wmpz4q2smpxlhqzj',
+      amountSat: '',
+      errMsg: '',
+      inFetch: false,
+      sendMax: ''
+    })
+    const amountEle = document.getElementById('amountToSend')
+    amountEle.value = ''
+
+    const addressEle = document.getElementById('addressToSend')
+    addressEle.value = ''
+  }
+
+  validateInputs () {
+    const { address, amountSat } = _this.state
+    const amountNumber = Number(amountSat)
+
+    if (!address) {
+      throw new Error('Address is required')
+    }
+
+    if (!amountSat) {
+      throw new Error('Amount is required')
+    }
+
+    if (!amountNumber) {
+      throw new Error('Amount must be a number')
+    }
+
+    if (amountNumber < 0) {
+      throw new Error('Amount must be greater than zero')
+    }
+  }
+
+  onHandleToggleScanner () {
+    _this.setState({
+      showScan: !_this.state.showScan
+    })
+  }
+
+  handleModal () {
+    _this.setState({
+      showScan: !_this.state.showScan
+    })
+  }
+
+  resetAddressValue () {
+    _this.setState({
+      address: '',
+      errMsg: ''
+    })
+    const addressEle = document.getElementById('addressToSend')
+    addressEle.value = ''
+  }
+
+  onHandleScan (data) {
+    try {
+      _this.resetAddressValue()
+      if (!data) {
+        throw new Error('No Result!')
+      }
+      if (typeof data !== 'string') {
+        throw new Error('It should scan a bch address or slp address')
+      }
+
+      _this.setState({
+        address: data,
+        errMsg: ''
+      })
+      const addressEle = document.getElementById('addressToSend')
+      addressEle.value = data
+
+      _this.onHandleToggleScanner()
+    } catch (error) {
+      _this.onHandleToggleScanner()
+      _this.setState({
+        errMsg: error.message
+      })
+    }
+  }
+
+  handleError (error) {
+    // console.error(error)
+    let errMsg = ''
+    if (error.message) {
+      errMsg = error.message
+    }
+    if (error.error) {
+      if (error.error.match('rate limits')) {
+        errMsg = (
+          <span>
+            Rate limits exceeded, increase rate limits with a JWT token from
+            <a
+              style={{ marginLeft: '5px' }}
+              target='_blank'
+              href='https://fullstack.cash'
+              rel='noopener noreferrer'
+            >
+              FullStack.cash
+            </a>
+          </span>
+        )
+      } else {
+        errMsg = error.error
+      }
+    }
+    _this.setState(prevState => {
+      return {
+        ...prevState,
+        errMsg,
+        txId: '',
+        inFetch: false
+      }
+    })
+  }
+}
+EncryptedChat.propTypes = {
+  updateBalance: PropTypes.func.isRequired,
+  bchWallet: PropTypes.object,
+  currentRate: PropTypes.number
 }
 
-export default TokenSwap
+export default TokenSWap
